13a35211642cd046961333d26fc70170c7c1f14c
SarangaR <sarnga.raj@gmail.com>
2022-03-15 20:05:23 -0400

    Made home page and tried login activity

--- app/src/main/java/com/helpinghand/app/MainActivity.java
+++ app/src/main/java/com/helpinghand/app/MainActivity.java
@@ -2,13 +2,31 @@ package com.helpinghand.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.login_button);
+        login.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login_button:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
     }
 }
